146300ef2570d90cf909bfe20ab3e81a0d77c459
denis <petrovsky.dv@gmail.com>
2023-02-24 14:00:13 +0300

    fix fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from pymorphy2 import MorphAnalyzer
 
@@ -11,8 +13,9 @@ def analyzer():
 
 @pytest.fixture(scope='session')
 def charged_words():
-    negative_words_path = '../charged_dict/negative_words.txt'
-    positive_words_path = '../charged_dict/positive_words.txt'
+    base_dir = Path(__file__).resolve().parent.parent
+    negative_words_path = base_dir / 'charged_dict/negative_words.txt'
+    positive_words_path = base_dir / 'charged_dict/positive_words.txt'
     charged_words = get_charged_words_from_file(negative_words_path)
     charged_words.extend(get_charged_words_from_file(positive_words_path))
     return charged_words
